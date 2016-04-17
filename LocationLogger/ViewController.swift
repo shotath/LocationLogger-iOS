6688157f8eb2aa3b56804845fe6f86adeb93c6bb
Shota Kurihara <shotath911@gmail.com>
2016-04-17 21:41:00 +0900

    プロジェクトの追加

---
+++ LocationLogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationLogger
+//
+//  Created by shota_th on 2016/04/17.
+//  Copyright © 2016年 s.kurihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
